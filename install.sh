5be4803a361bf04c3f06c793dbb2ffa9a2a17346
gandia <xufongnian@gmail.com>
2020-11-05 21:44:50 +0800

    install shell push
    Signed-off-by: gandia <xufongnian@gmail.com>

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo go get -u github.com/golang/protobuf/protoc-gen-go
+sudo go get github.com/micro/protoc-gen-micro
+sudo go get github.com/micro/go-micro/v2@latest
+sudo go get github.com/micro/micro/v2@latest
